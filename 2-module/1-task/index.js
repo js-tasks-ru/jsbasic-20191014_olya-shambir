b0f1d49287a097889c7b7b83daff0074854efff0
olya <olgashambir@gmail.com>
2019-10-27 14:08:50 +0200

    objects 1st task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,16 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+
 function clone(obj) {
+  // eslint-disable-next-line no-shadow
+  const cloneObj = {};
+  for (const key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      cloneObj[key] = clone(obj[key]);
+    } else {
+      cloneObj[key] = obj[key];
+    }
+  }
+  return cloneObj;
 }
