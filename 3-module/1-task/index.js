09fe87efb774412cfc7d6d2c658f7e79831993a3
olya <olgashambir@gmail.com>
2019-11-05 21:16:22 +0200

    3rd module-1st task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -2,7 +2,16 @@
  * showSalary
  * @param {Array} data - данные о пользователях
  * @param {number} age - максимальный возраст
- * @returns {string}
+ * @returns {Array}
  */
+
+// eslint-disable-next-line no-shadow
 function showSalary(data, age) {
+  const users = [];
+  for (const item of data) {
+    if (item.age <= age) {
+      // eslint-disable-next-line no-shadow
+      users.push(`${item.name}, ${item.balance}`);
+    }
+  } return users.join('\n');
 }
